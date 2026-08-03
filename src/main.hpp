ed4f351ed0dca00e53c5776faed4d8c8aea17925
Labmem-015 <naumov.vlad04@gmail.com>
2026-08-03 15:22:21 +0300

    Format main.hpp

--- src/main.hpp
+++ src/main.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
-#include <print>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <cassert>
-#include <cstdint>
 #include <atomic>
+#include <cassert>
 #include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <print>
+#include <sstream>
 #include <stop_token>
+#include <string>
 #include <thread>
+#include <vector>
 
 #include <raylib.h>
 
 #define CL_HPP_TARGET_OPENCL_VERSION 300 // Specifies target OpenCL version
-#include <CL/opencl.hpp> // Core OpenCL C header
+#include <CL/opencl.hpp>                 // Core OpenCL C header
